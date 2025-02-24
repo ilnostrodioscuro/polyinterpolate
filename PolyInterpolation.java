59d4cd99282ffdd14005721c4f63a96dd8282223
ray <122627494+ilnostrodioscuro@users.noreply.github.com>
2025-02-24 12:23:40 -0500

    Create PolyInterpolation.java

---
+++ PolyInterpolation.java
@@ -0,0 +1,38 @@
+/*
+ * Ray Murdorf
+ * 1/27/2025
+ * Applied CS Math
+ * Polynomial Interpolation
+ * 
+ * Finds a polynomial going through all points in a given dataset using interpolation
+ * the array of points starts at 0, so the first value should be x0 not x1
+ */
+
+ import java.util.*; // scanner, arraylist
+
+public class PolyInterpolation {
+    public static double divDiff(double points[][], int x, int y) { // returns a at the given depth
+        // a1 = points[0][1] = f(x0)
+        if(y == x) return points[x][1]; // returns f[xi]
+        else return (divDiff(points, x, y - 1) - divDiff(points, x + 1, y)) / (points[y][0] - points[x][0]);
+    }
+
+    public static String inter(double points[][]) {
+        //
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.print("How many x values do you have? ");
+        int iter = kb.nextInt();
+        double[][] points = new double[iter][2];
+        for(int i = 0; i < iter; i++) {
+            System.out.printf("x%d? ", i + 1);
+            points[i][0] = kb.nextDouble();
+            System.out.printf("y%d? ", i + 1);
+            points[i][1] = kb.nextDouble();
+        }
+        kb.close();
+    }
+}
